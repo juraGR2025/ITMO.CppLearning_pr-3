f304a4ef69976aa73d24369da44b060ab572a7f9
juraGR2025 <93720304+juraGR2025@users.noreply.github.com>
2022-06-23 21:59:54 +0300

    Add files via upload

---
+++ Cpp_practice_3-2/Cpp_practice_3-2.cpp
@@ -0,0 +1,34 @@
+﻿// ITMO.CppLearning_pr-3.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string privet(string name)
+{
+	string str = name + ", " + "hello!\n";
+	return str;
+}
+/*В проекте упражнения 1 добавьте(не замените имеющуюся, а именно
+	добавьте новую) вторую функцию privet(), но с двумя параметрами.*/
+void privet(string name, int k)
+{
+	cout << name << ", " << "hello! " << "you input " << k << endl;
+}
+int main()
+{
+	string name;
+	int k;/*В функции main() объявите целочисленную переменную и реализуйте
+ввод ее значения:*/
+	cout << "Input number:" << endl;
+	cin >> k;
+	cout << "What is your name?" << endl;
+	cin >> name;
+	string nameOut = privet(name);
+	cout << nameOut << endl;
+	privet(name);
+	privet(name, k);
+	return 0;
+}
