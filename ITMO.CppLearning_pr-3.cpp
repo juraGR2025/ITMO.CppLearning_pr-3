a13f6f4721b6b1d5383b62c57c4aa957376ad906
juraGR2025 <93720304+juraGR2025@users.noreply.github.com>
2022-06-23 21:53:30 +0300

    Добавьте файлы проекта.

---
+++ ITMO.CppLearning_pr-3.cpp
@@ -0,0 +1,23 @@
+﻿// ITMO.CppLearning_pr-3.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+#include <iomanip>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void privet(string);
+
+int main()
+{
+	string name;
+	cout << "What is your name?" << endl;
+	cin >> name;
+	privet(name);
+	return 0;
+}
+
+void privet(string name)
+{
+	cout << name << ", " << "hello!" << endl;
+}
